35a03dd858a8c53e6458e512b7c8eda87f3f7bdc
Yao Gameli AMEGANDJIN <yaogameliamegandjin@gmail.com>
2023-10-11 14:05:27 +0000

    Deuxième version: optimisation du code

--- main.py
+++ main.py
@@ -1,20 +1,53 @@
-import os # pour la manipulation des fichiers
+import os
 
-chemin = "img/"
-def main():
+# Initialize the variable to False, which will be used to control the while loop.
+saisie_correct = False
+
+# Start a while loop that will continue until saisie_correct is True.
+while not saisie_correct:
+    # Prompt the user to enter the path of the folder containing the files to rename.
+    chemin = input("Veuillez saisir le chemin du dossier qui contient les fichier à renommer : ")
+    
+    # Replace backslashes with forward slashes in the path.
+    chemin = chemin.replace("\\", "/")
+    
+    # Check if the last character of the path is not a forward slash.
+    if chemin[-1] != "/":
+        # If not, append a forward slash to the path.
+        chemin += "/"
+    
+    # Check if the path is a valid directory and if it is not empty.
+    if os.path.isdir(chemin) and os.listdir(chemin) != []:
+        # If so, set saisie_correct to True to exit the while loop.
+        saisie_correct = True
+    else:
+        # If not, print an error message indicating that the path is invalid.
+        print("Le chemin saisi n'est pas valide. \n Le dossier est vide \n ou ce n'est pas un dossier \n ou le chemin spécifié n'existe pas.")
+        
+def rename_images(chemin):
     """
-    Renomme les images dans le sous-dossier img.
+    Rename the images in the 'img' subdirectory.
+    
+    Args:
+        chemin (str): The path to the 'img' subdirectory.
     """
+    # Initialize counter
     i = 0
-    dossier_test = f'{os.getcwd()}/{chemin}'
-    #print(dossier_test)
-    for filename in os.listdir(dossier_test):
-        nouveau_nom = f'{dossier_test}image_test{i}.jpg'
-        #print(nouveau_nom)
-        ancien_nom = f'{dossier_test}{filename}'
-        #print(ancien_nom)
+    
+    # Iterate over each file in the 'img' subdirectory
+    for filename in os.listdir(chemin):
+        # Generate the new name for the image file
+        nouveau_nom = os.path.join(chemin, f'image_test{i}.jpg')
+        
+        # Generate the old name of the image file
+        ancien_nom = os.path.join(chemin, filename)
+        
+        # Rename the image file
         os.rename(ancien_nom, nouveau_nom)
+        
+        # Increment the counter
         i += 1
+    print("Les fhichiers ont bien été renommées.")
 
 if __name__ == "__main__":
-    main()
+    rename_images(chemin)
